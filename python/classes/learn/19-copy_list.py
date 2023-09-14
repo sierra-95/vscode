d385bdafda81580ca890e02a0845458109c1e2d6
sierra-95 <michaelmachohi@gmail.com>
2023-09-14 18:10:25 +0300

    m

---
+++ python/classes/learn/19-copy_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#this is under the project 0x09 Python : Everything is object
+def copy_list(l):
+    new = []
+    for i in l:
+        new.append(i)
+    return new    
