eaef4c5957191801d1ba3a1dd1aa0f7fbce06be6
sierra-95 <michaelmachohi@gmail.com>
2023-09-27 13:54:27 +0300

    m

--- python/almost_a_circle/models/rectangle.py
+++ python/almost_a_circle/models/rectangle.py
@@ -54,11 +54,13 @@ class Rectangle(Base,BaseGeometry):
     def __str__(self):
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
     def update(self,*args):
-        list=[]
-        for i in args:
-            list.append(i)
-        self.id=list[0]
-        self.__width=list[1]
-        self.__height=list[2]
-        self.__x=list[3]
-        self.__y=list[4]
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
--- python/almost_a_circle/test.py
+++ python/almost_a_circle/test.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python3
 def update(*args):
-    list=[]
-    for i in args:
-        list.append(i)
-    a=list[0]
-    b=list[1]
-    c=list[2]
-    print(a,b,c)
-update(4,5,6)
+    # Initialize variables with None
+    a, b, c, d, e = None, None, None, None, None
+
+    # Assign values from args to variables
+    if len(args) >= 1:
+        a = args[0]
+    if len(args) >= 2:
+        b = args[1]
+    if len(args) >= 3:
+        c = args[2]
+    if len(args) >= 4:
+        d = args[3]
+    if len(args) >= 5:
+        e = args[4]
+
+    # Print all variables
+    print(a, b, c, d, e)
+
+# Test cases
+update(4)               # Stores 4, leaves b, c, d, e as None
+update(4, 5)            # Stores 4 and 5, leaves c, d, e as None
+update(4, 5, 6, 7, 8)   # Stores all 5 values
